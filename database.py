eb34579aa0144a9f19d480089dec0e6e2b055591
Bhavana Sherekar <sherekarparasharam02@gmail.com>
2026-04-08 20:50:48 +0530

    updated database config for cloud2

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 import os
-DATABASE_URL = os.getenv("postgresql://postgres:[YOUR-PASSWORD]@db.kbubdubbrjgvycqegvux.supabase.co:5432/postgres")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
     DATABASE_URL,
